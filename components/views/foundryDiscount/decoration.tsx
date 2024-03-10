5b5b2db197823956290ec13e9dcf14290e2dd7e8
Liroo <liroo.pierre@gmail.com>
2024-03-10 19:12:27 +0100

    Add few animation and optimize discount images

--- components/views/foundryDiscount/decoration.tsx
+++ components/views/foundryDiscount/decoration.tsx
@@ -12,32 +12,32 @@ export default function FoundryDiscountDecoration() {
       <Image
         src={LeafBottom1Png}
         alt="chat"
-        className="absolute bottom-0 left-[20%] w-[119px]"
+        className="absolute bottom-0 left-[20%] w-[119px] origin-bottom animate-[animation-leaf_15s_ease-in-out_infinite]"
       />
       <Image
         src={LeafBottom2Png}
         alt="chat"
-        className="absolute bottom-0 right-[20%] w-[87px]"
+        className="absolute bottom-0 right-[20%] w-[87px] origin-bottom-left animate-[animation-leaf_12s_ease-in-out_infinite]"
       />
       <Image
         src={LeafLeftPng}
         alt="chat"
-        className="absolute left-0 top-[5%] w-[110px] laptop:top-[10%] laptop:block laptop:w-[206px]"
+        className="absolute left-0 top-[5%] w-[110px] origin-bottom-left animate-[animation-leaf_20s_ease-in-out_infinite] laptop:top-[10%] laptop:block laptop:w-[206px]"
       />
       <Image
         src={LeafRightPng}
         alt="chat"
-        className="absolute right-0 top-[5%] w-[90px] laptop:top-[10%] laptop:block laptop:w-[182px]"
+        className="absolute right-0 top-[5%] w-[90px] origin-right animate-[animation-leaf_18s_ease-in-out_infinite] laptop:top-[10%] laptop:block laptop:w-[182px]"
       />
       <Image
         src={TournesolLeftPng}
         alt="chat"
-        className="absolute bottom-[20%] left-0 hidden w-[192px] laptop:block"
+        className="absolute bottom-[20%] left-0 hidden w-[192px] origin-bottom-left animate-[animation-leaf_23s_ease-in-out_infinite] laptop:block"
       />
       <Image
         src={TournesolRightPng}
         alt="chat"
-        className="absolute bottom-[25%] right-0 hidden w-[202px] laptop:block"
+        className="absolute bottom-[25%] right-0 hidden w-[202px] origin-bottom-right animate-[animation-leaf_25s_ease-in-out_infinite] laptop:block"
       />
     </>
   );
--- components/views/foundryDiscount/index.tsx
+++ components/views/foundryDiscount/index.tsx
@@ -1,13 +1,44 @@
 import { postApiDiscount } from 'flux/foundry/action';
 import { useAppDispatch } from 'flux/store';
 import RightArrowSvg from 'icons/right-arrow.svg';
+import ChatPng from 'images/foundry/discount/chat.png';
+import DragonPng from 'images/foundry/discount/dragon.png';
+import GameboyPng from 'images/foundry/discount/gameboy.png';
+import LeafBottom1Png from 'images/foundry/discount/leaf-bottom-1.png';
+import LeafBottom2Png from 'images/foundry/discount/leaf-bottom-2.png';
+import LeafLeftPng from 'images/foundry/discount/leaf-left.png';
+import LeafRightPng from 'images/foundry/discount/leaf-right.png';
 import MachinePng from 'images/foundry/discount/machine.png';
+import PhoPng from 'images/foundry/discount/pho.png';
+import Sinnoh1Png from 'images/foundry/discount/sinnoh-1.png';
+import Sinnoh2Png from 'images/foundry/discount/sinnoh-2.png';
+import TournesolLeftPng from 'images/foundry/discount/tournesol-left.png';
+import TournesolRightPng from 'images/foundry/discount/tournesol-right.png';
+import VasePng from 'images/foundry/discount/vase.png';
 import { useEffect, useRef, useState } from 'react';
 import { DiscountSymbols } from 'utils/constants';
 import FoundryDiscountColumn, { FoundryDiscountColumnHandle } from './column';
 import FoundryDiscountDecoration from './decoration';
 
+const images = [
+  MachinePng,
+  LeafBottom1Png,
+  LeafBottom2Png,
+  LeafLeftPng,
+  LeafRightPng,
+  TournesolLeftPng,
+  TournesolRightPng,
+  ChatPng,
+  DragonPng,
+  GameboyPng,
+  PhoPng,
+  Sinnoh1Png,
+  Sinnoh2Png,
+  VasePng,
+];
+
 export default function FoundryDiscountView() {
+  const [progress, setProgress] = useState<number>(0);
   const dispatch = useAppDispatch();
   const machineRef = useRef<HTMLImageElement>(null);
   const [[machineWidth, machineHeight], setMachineSize] = useState<
@@ -77,43 +108,63 @@ export default function FoundryDiscountView() {
     }
   };
 
+  useEffect(() => {
+    let imageLoaded = 0;
+    images.forEach((image) => {
+      const img = new Image();
+      img.onload = () => {
+        imageLoaded += 1;
+        setProgress((imageLoaded / images.length) * 100);
+      };
+      img.src = image.src;
+    });
+  }, []);
+
   return (
     <div className="pointer-events-none flex-1 overflow-hidden">
-      <div className="relative h-full laptop:py-[30px]">
-        <div className="relative h-full">
-          <img
-            onLoad={(e) => {
-              getImageSize(e.target as HTMLImageElement);
-            }}
-            ref={machineRef}
-            src={MachinePng.src}
-            alt="chat"
-            className="mx-auto h-full w-full object-contain"
-          />
-          <button
-            className="pointer-events-auto absolute left-[calc(50vw+30vh)] top-[45%] flex h-[20px] w-[20px] select-none items-center justify-center rounded-full bg-[#B4AFAB] font-romie text-[12px] uppercase text-white outline-none laptop:h-auto laptop:w-auto laptop:px-[20px] laptop:py-[6px] portrait:left-auto portrait:right-[8vw] portrait:top-[calc(50%-7vw)]"
-            onClick={onClickPull}
-          >
-            <RightArrowSvg className="w-[10px] rotate-90 fill-current text-white laptop:hidden" />
-            <p className="hidden laptop:block">Pull</p>
-          </button>
+      {progress >= 100 ? (
+        <div className="relative h-full laptop:py-[30px]">
+          <div className="relative h-full">
+            <img
+              onLoad={(e) => {
+                getImageSize(e.target as HTMLImageElement);
+              }}
+              ref={machineRef}
+              src={MachinePng.src}
+              alt="chat"
+              className="mx-auto h-full w-full object-contain"
+            />
+            <button
+              className="pointer-events-auto absolute left-[calc(50vw+30vh)] top-[45%] flex h-[20px] w-[20px] select-none items-center justify-center rounded-full bg-[#B4AFAB] font-romie text-[12px] uppercase text-white outline-none laptop:h-auto laptop:w-auto laptop:px-[20px] laptop:py-[6px] portrait:left-auto portrait:right-[8vw] portrait:top-[calc(50%-7vw)]"
+              onClick={onClickPull}
+            >
+              <RightArrowSvg className="w-[10px] rotate-90 fill-current text-white laptop:hidden" />
+              <p className="hidden laptop:block">Pull</p>
+            </button>
 
-          <div
-            className="absolute z-10 grid grid-cols-3 gap-[3%]"
-            style={{
-              width: `${machineWidth * 0.522}px`,
-              height: `${machineHeight * 0.35}px`,
-              top: `calc(50% - ${machineHeight * 0.304}px)`,
-              left: `calc(50% - ${machineWidth * 0.26}px)`,
-            }}
-          >
-            <FoundryDiscountColumn ref={line1Ref} />
-            <FoundryDiscountColumn ref={line2Ref} />
-            <FoundryDiscountColumn ref={line3Ref} />
+            <div
+              className="absolute z-10 grid grid-cols-3 gap-[3%]"
+              style={{
+                width: `${machineWidth * 0.522}px`,
+                height: `${machineHeight * 0.35}px`,
+                top: `calc(50% - ${machineHeight * 0.304}px)`,
+                left: `calc(50% - ${machineWidth * 0.26}px)`,
+              }}
+            >
+              <FoundryDiscountColumn ref={line1Ref} />
+              <FoundryDiscountColumn ref={line2Ref} />
+              <FoundryDiscountColumn ref={line3Ref} />
+            </div>
           </div>
+          <FoundryDiscountDecoration />
         </div>
-        <FoundryDiscountDecoration />
-      </div>
+      ) : (
+        <div className="flex h-full items-center justify-center">
+          <p className="text-center font-romie text-[45px]">
+            {Math.round(progress)}
+          </p>
+        </div>
+      )}
     </div>
   );
 }
