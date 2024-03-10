5baaf359e90f0b48d9f5b0a9a4a9d74b712e82f6
Liroo <liroo.pierre@gmail.com>
2024-03-10 11:21:06 +0100

    Use single deployment

--- services/firebase/index.ts
+++ services/firebase/index.ts
@@ -3,10 +3,8 @@ import { cert, getApps, initializeApp } from 'firebase-admin/app';
 export default function initializeFirebase() {
   if (getApps().length <= 0)
     initializeApp({
-      credential: cert({
-        projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
-      }),
+      credential: cert(
+        JSON.parse(process.env.FIREBASE_ADMIN_SERVICE_ACCOUNT as string),
+      ),
     });
 }
